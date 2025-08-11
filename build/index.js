40ff8e06ff6f0aa4b2681c5dfc8ec715bc63c7b1
逸清 <dyq02122503@alibaba-inc.com>
2025-08-11 10:44:17 +0800

    Add shebang to make executable

--- build/index.js
+++ build/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { CallToolRequestSchema, GetPromptRequestSchema, ListPromptsRequestSchema, ListToolsRequestSchema, ListResourcesRequestSchema, ReadResourceRequestSchema, } from "@modelcontextprotocol/sdk/types.js";
